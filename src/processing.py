d01f4edbce83015670a989227af1eee08d9b062e
VTokyoGhoulV <0hellnomad0@gmail.com>
2026-04-11 20:02:31 +0300

    processing update (add filter_by_state)

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,8 @@
-def filter_by_state(dict_list: list, state: str = "EXECUTED"):
-    pass
+def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
+    filtered_list = []
+
+    for item in dict_list:
+        if item["state"] == state:
+            filtered_list.append(item)
+
+    return filtered_list

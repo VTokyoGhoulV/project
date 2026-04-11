87a958cfa0d0fe02fe757ce5820cb3084a3048c2
VTokyoGhoulV <0hellnomad0@gmail.com>
2026-04-11 23:11:21 +0300

    upddate processing

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
 def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
+    """
+    Принимает список словарей и возвращает отфильтрованный список по состоянию
+    """
     filtered_list = []
 
     for item in dict_list:
@@ -8,5 +11,8 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
     return filtered_list
 
 
-def sort_by_date(dict_list: list) -> list:
-    return sorted(dict_list, key=lambda x: x["date"])
+def sort_by_date(dict_list: list, reversed: bool = True) -> list:
+    """
+    Принимает список словарей и возвращает отсортированный список по дате
+    """
+    return sorted(dict_list, reverse=reversed, key=lambda x: x["date"])

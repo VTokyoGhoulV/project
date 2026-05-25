12a91fc1bf73f4965eac9b17e8cd5c4ab49256f6
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-05-25 16:07:55 +0300

    add process_bank_search, process_bank_operations

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,7 @@
+import re
+from collections import Counter
+
+
 def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
     """
     Принимает список словарей и возвращает отфильтрованный список по состоянию
@@ -5,7 +9,7 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
     filtered_list = []
 
     for item in dict_list:
-        if item["state"] == state:
+        if item.get("state") == state:
             filtered_list.append(item)
 
     return filtered_list
@@ -16,3 +20,19 @@ def sort_by_date(dict_list: list, is_reversed: bool = True) -> list:
     Принимает список словарей и возвращает отсортированный список по дате
     """
     return sorted(dict_list, reverse=is_reversed, key=lambda x: (x.get("date") is None, x.get("date")))
+
+
+def process_bank_search(data: list, search: str) -> list[dict]:
+    """Принимает список словарей и возвращает список отфильтрованный по заданным словам"""
+
+    result = [item for item in data if re.search(search, item.get("description"), re.IGNORECASE)]
+
+    return result
+
+
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """принимает список и возвращает словарь с количеством повторов категорий транзакций"""
+
+    category_counts = Counter(transaction.get("description") for transaction in data)
+
+    return {cat: category_counts.get(cat, 0) for cat in categories}
